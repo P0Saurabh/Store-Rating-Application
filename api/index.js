5bfb3fd6e0862db65cd1aeafe1cc8601d42e52b1
Sakshi443 <mmanoorkar9@gmail.com>
2025-12-18 01:12:09 +0530

    fix: spa routing and error handling

--- api/index.js
+++ api/index.js
@@ -22,4 +22,13 @@ app.get('/', (req, res) => {
     res.send('API is running...');
 });
 
+// Global Error Handler
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).json({
+        message: 'Internal Server Error',
+        error: process.env.NODE_ENV === 'production' ? {} : err.message
+    });
+});
+
 module.exports = app;
