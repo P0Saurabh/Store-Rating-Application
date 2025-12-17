9a62811efc1fde4102b98402909d6b45eb551ae9
Sakshi443 <mmanoorkar9@gmail.com>
2025-12-17 23:41:41 +0530

    refactor: migrate backend to Vercel serverless functions

--- api/index.js
+++ api/index.js
@@ -1,3 +1,25 @@
-const app = require('../server/server.js');
+const express = require('express');
+const cors = require('cors');
+const dotenv = require('dotenv');
+const { connectDB } = require('../server/config/db');
+
+dotenv.config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+const authRoutes = require('../server/routes/authRoutes');
+
+// Connect to Database
+connectDB();
+
+app.use('/api/auth', authRoutes);
+app.use('/api', require('../server/routes/dataRoutes'));
+
+app.get('/', (req, res) => {
+    res.send('API is running...');
+});
 
 module.exports = app;
