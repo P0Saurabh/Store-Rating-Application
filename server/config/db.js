8c0685fe5747a30036666efa004ff79268e6e7a0
Sakshi443 <mmanoorkar9@gmail.com>
2025-12-17 23:52:57 +0530

    fix: switch to postgres for supabase support

--- server/config/db.js
+++ server/config/db.js
@@ -5,7 +5,7 @@ const path = require('path');
 dotenv.config({ path: path.join(__dirname, '../.env') });
 
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
-    dialect: 'mysql',
+    dialect: 'postgres',
     logging: false, // Set to console.log to see SQL queries
     dialectOptions: {
         ssl: {
